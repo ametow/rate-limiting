6547297fe5ef036c17636e4d87169ee075190d04
Arslan Ametov <arsinux@yandex.com>
2025-04-01 00:04:34 -0500

    bug with passing rate limiter

--- main.go
+++ main.go
@@ -17,13 +17,13 @@ func getClientIP(r *http.Request) string {
 	return host
 }
 
-func rateLimiterMiddleware(next http.Handler, rateLimiter limiter.RateLimiter) http.Handler {
+func rateLimiterMiddleware(next http.Handler) http.Handler {
 	ipLimiterMap := sync.Map{}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ip := getClientIP(r)
 
-		ipLimiter, _ := ipLimiterMap.LoadOrStore(ip, rateLimiter)
+		ipLimiter, _ := ipLimiterMap.LoadOrStore(ip, limiter.NewTokenBucketLimiter(2.0, 10))
 
 		if !ipLimiter.(limiter.RateLimiter).Allow() {
 			w.WriteHeader(http.StatusTooManyRequests)
@@ -42,8 +42,8 @@ func main() {
 		w.Write([]byte("hi there\n"))
 	})
 
-	// handler := rateLimiterMiddleware(mux, limiter.NewSlidingWindowLimiter(1, 2))
-	handler := rateLimiterMiddleware(mux, limiter.NewTokenBucketLimiter(3.0, 10))
+	// handler := rateLimiterMiddleware(mux)
+	handler := rateLimiterMiddleware(mux)
 
 	err := http.ListenAndServe("127.0.0.1:8080", handler)
 	if err != nil {
